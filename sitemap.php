f30a42730a99a1556d65406f27e3d078e1ee738a
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2024-10-21 18:29:28 +0200

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -13,7 +13,7 @@ $posts = explode(PHP_EOL, trim($listContent));
 $pages = [];
 
 foreach ($posts as $post) {
-    $url = 'https://crm.indopedia.co.id/?post=' . trim($post);
+    $url = 'https://stikespanritahusada.ac.id/?post=' . trim($post);
     $pages[] = [
         'loc' => $url,
         'lastmod' => date('Y-m-d'),
